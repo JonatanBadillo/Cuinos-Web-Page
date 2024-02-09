34848b87ed393e2ecf627872455d481fecac679c
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2024-02-09 08:47:42 -0600

    changing style background

--- main.js
+++ main.js
@@ -1,19 +1,4 @@
-///////////////////////////banner and nav//////////////////////////////////////////////
-document.getElementById("icon_menu").addEventListener("click", mostrar_menu);
 
-function mostrar_menu(){
-
-    document.querySelector(".menu").classList.toggle("mostrar_menu");
-    
-}
-
-window.onscroll = function () {
-    var posicion = window.pageYOffset || document.documentElement.scrollTop;
-    var elemento1 = document.getElementById("icon_ball");
-    var elemento2 = document.getElementById("icon_player");
-    elemento1.style.bottom = posicion * 0.1 + "px";
-    elemento2.style.top = posicion * 0.1 + "px";
-}
 //////////////////////////////////////  table  //////////////////////////////////////
 
 const search = document.querySelector('.input-group input'),
