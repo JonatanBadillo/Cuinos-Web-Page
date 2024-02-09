3cac9486f82d43dad609e54e9a95fc914748ef33
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2024-02-09 08:48:58 -0600

    Añadir funcionalidad de menú y animación de desplazamiento

--- main.js
+++ main.js
@@ -133,3 +133,23 @@ document.addEventListener('click', (e) => {
         e.target.style.display = 'none';
     }
 });
+
+//////////
+///////////////////////////banner and nav//////////////////////////////////////////////
+/*
+document.getElementById("icon_menu").addEventListener("click", mostrar_menu);
+
+function mostrar_menu(){
+
+    document.querySelector(".menu").classList.toggle("mostrar_menu");
+    
+}
+
+*/
+window.onscroll = function () {
+    var posicion = window.pageYOffset || document.documentElement.scrollTop;
+    var elemento1 = document.getElementById("icon_ball");
+    var elemento2 = document.getElementById("icon_player");
+    elemento1.style.bottom = posicion * 0.1 + "px";
+    elemento2.style.top = posicion * 0.1 + "px";
+}
