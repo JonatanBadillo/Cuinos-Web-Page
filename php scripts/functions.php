ed00b3866c6f63a4d466c371a752723b847c6106
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2024-02-10 03:01:47 -0600

    aded some functions

--- php scripts/functions.php
+++ php scripts/functions.php
@@ -1,1 +1,11 @@
 <?php
+function splitter($urls, $divisor)
+{
+    $strings_array = array();
+    $strings_array = explode($divisor, $urls);
+    return $strings_array; // Usar como $data = split_urls($cadena_con_urls); (Validar con length)
+}
+function get_last_insert_id($connection)
+{
+    return $connection->insert_id;
+}
