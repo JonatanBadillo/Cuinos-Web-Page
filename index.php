6d9a1da57954d710337afd833b926e9d259ace11
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2024-02-10 23:39:33 -0600

    fixed function

--- index.php
+++ index.php
@@ -88,7 +88,7 @@
                     for ($j = 0; $j < sizeof($playerPositions); $j++) {
                         ($j == 0) ? $textPositions .= strval($positions[$playerPositions[$j]]) : $textPositions .= (" / " . strval($positions[$playerPositions[$j]]));
                     }
-                    $playerDOM = substr_replace($playerDOM, "POSITION", $textPositions);
+                    $playerDOM = str_replace("POSITION", $textPositions, $playerDOM);
                 }
             }
             echo ($playerDOM);
