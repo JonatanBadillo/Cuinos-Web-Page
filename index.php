1ce8e51ac4f5b0eba2ad5db559b0d59cf80e6a8e
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2024-02-11 03:30:38 -0600

    finished debugging

--- index.php
+++ index.php
@@ -85,7 +85,7 @@
         for ($i = 0; $i < $n; $i++) {
             ($i == 0) ? $positions = ["", $positions_fetch[$i][1]] : array_push($positions, $positions_fetch[$i][1]);
         }
-        print_r($positions);
+        //print_r($positions);
         $indexes = [0, 0, 1, 2, 3, 7, 6, 8];
         for ($i = 0; $i < sizeof($players); $i++) {
             $playerDOM = flag_replacer($dynamicDOM, "FLAG", $players[$i], $indexes);
