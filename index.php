9afc4ca40ee0b5c02e872ed9ac95dfaaa914155a
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2024-02-20 16:13:19 -0600

    Cambiar descripción a "HOLA" en index.php

--- index.php
+++ index.php
@@ -104,7 +104,7 @@
                                                <path d="m16.192 6.344-4.243 4.242-4.242-4.242-1.414 1.414L10.535 12l-4.242 4.242 1.414 1.414 4.242-4.242 4.243 4.242 1.414-1.414L13.364 12l4.242-4.242z"></path>
                                            </svg>
                                        </button>
-                                       <p>DESCRIPTION</p>
+                                       <p>HOLA</p>
                                     </div>
                                </div>
                            </div>
@@ -131,8 +131,7 @@
                 $k++;
             }
             $playerDOM = str_replace("POSITION", $text_positions, $playerDOM);
-
-			$playerDOM = str_replace("DESCRIPTION", $players[$i][4] , $playerDOM);
+			$playerDOM = str_replace("HOLA", $players[$i][4] , $playerDOM);
 
 
             echo ($playerDOM);
