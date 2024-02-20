1c4fdc68327aa5a13d88394354fd68c92c5e785c
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2024-02-20 13:47:55 -0600

    Actualizado el texto de descripción en index.php

--- index.php
+++ index.php
@@ -104,7 +104,7 @@
                                                <path d="m16.192 6.344-4.243 4.242-4.242-4.242-1.414 1.414L10.535 12l-4.242 4.242 1.414 1.414 4.242-4.242 4.243 4.242 1.414-1.414L13.364 12l4.242-4.242z"></path>
                                            </svg>
                                        </button>
-                                       <p>DESCRIPTION_FLAG</p>
+                                       <p>HOLA</p>
                                        </div>
                                </div>
                            </div>
@@ -122,7 +122,6 @@
         $indexes = [0, 0, 1, 2, 3, 7, 6, 8];
         for ($i = 0; $i < sizeof($players); $i++) {
             $playerDOM = flag_replacer($dynamicDOM, "FLAG", $players[$i], $indexes);
-			$playerDOM = str_replace("DESCRIPTION_FLAG", $players[$i][4], $playerDOM); 
             $playerPositions = splitter($players[$i][5], ",");
             // print_r($playerPositions);
             $text_positions = "";
